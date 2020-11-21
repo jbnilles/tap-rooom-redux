cff24b7b822498d9a41b1767292ef2f47ff0e6eb
jbnilles <jbnilles24@gmail.com>
2020-11-21 14:08:59 -0800

    fix bug relating to editing a keg

--- src/components/DetailsKeg.js
+++ src/components/DetailsKeg.js
@@ -16,18 +16,26 @@ function DetailsKeg(props){
     props.onChangeKegQuantityClick({
       Name: keg.Name, 
       Quantity: isSub ? (keg.Quantity === 0 ? 0 : keg.Quantity = parseInt(keg.Quantity) - dif) : keg.Quantity = parseInt(keg.Quantity) + dif, 
+      Price: keg.Price,
+      AlcoholContent: keg.AlcoholContent,
+      Brand: keg.Brand,
       id: keg.id});
+  }
+  const styleComponent = {
+    color : keg.Quantity > 50 ? "green" : keg.Quantity > 10 ? "orange" : "red"
   }
 
   return (
+
+    
     <React.Fragment>
       <h1>Keg Detail</h1>
       <h3>Name: {keg.Name}</h3>
       <h3>Brand: {keg.Brand}</h3>
       <h2>{quantity <=10 ? "Almost Empty" : ""}</h2>
-      <h3>Quantity: {quantity}</h3>
-      <h5>Price: {keg.Price}</h5>
-      <h5>Alcohol Percent: {keg.AlcoholContent}</h5>
+      <h3>Quantity: {quantity} Pints</h3>
+      <h5 style={styleComponent}>Price: ${keg.Price}</h5>
+      <h5>Alcohol Percent: {keg.AlcoholContent}%</h5>
       <button onClick = {()=> handleChangeKegQuantityButton(keg, true, 1) }>Buy</button> 
       <button onClick = { props.onClickingEdit }>Edit Keg</button>
       <button onClick = {() => onClickingDelete(keg.id) }>Remove Keg</button>
